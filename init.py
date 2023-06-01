843f43108d8c523d23d5e0dab179bf269904ca28
Thinkiomkar <132989245+Thinkiomkar@users.noreply.github.com>
2023-06-01 10:42:28 +0530

    Update init.py

--- init.py
+++ init.py
@@ -1,8 +1,7 @@
-from flask import Flask
+from flask import Flask, jsonify
 import pyodbc
 import pandas as pd
 import plotly.graph_objects as go
-import plotly.offline as py
 from plotly.tools import make_subplots
 
 server = 'ttplsqleu.database.windows.net'
@@ -22,13 +21,13 @@ def main():
     columns = [column[0] for column in cursor.description]
     result_reshaped = [tuple(row) for row in result]
     df = pd.DataFrame(result_reshaped, columns=columns)
+    json_data = df.to_json(orient='split')
     cursor.close()
     conn.close()
 
     fig = make_subplots(rows=1, cols=1, vertical_spacing=0.1, subplot_titles=(
         'Leads Status',
     ))
-
     T1 = go.Histogram(x=df['ls_description'])
     T1.name = 'Leads Status'
     fig.add_trace(T1, row=1, col=1)
@@ -36,10 +35,11 @@ def main():
         height=500, 
         width=1200   
     )
-    fig_html = py.plot(fig, output_type='div', include_plotlyjs='cdn')
-    fig_html = py.plot(fig, output_type='div', include_plotlyjs='cdn', config={'displayModeBar': False})
-    return fig_html 
+    fig.to_dict()['data'][0]['x'] =fig.to_dict()['data'][0]['x'].tolist()
+  
+    return jsonify({'data': json_data})
 
 if __name__ == '__main__':
     app.run(debug=True)
+
 
