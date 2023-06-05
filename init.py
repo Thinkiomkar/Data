f5da776fec131cb2efb2f1ea3d5a15713eb19524
Thinkiomkar <132989245+Thinkiomkar@users.noreply.github.com>
2023-06-05 10:35:33 +0530

    Update init.py

--- init.py
+++ init.py
@@ -1,9 +1,10 @@
 from flask import Flask, jsonify,request
 import pyodbc
 import pandas as pd
+import plotly.offline as py
 import plotly.graph_objects as go
-import json
 from plotly.tools import make_subplots
+
 
 server = 'ttplsqleu.database.windows.net'
 database = 'rms_live'
@@ -14,18 +15,22 @@ connection_string = f"DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={server};D
 app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def main():
-    cmsyscode = int(request.args.get('cmsyscode'))
-    fromdate=input(request.args.get('fromdate'))
-    todate=input(request.args.get('todate'))
+    # cmsyscode =input(request.args.get('cmsyscode'))
+    # fromdate=input(request.args.get('fromdate'))
+    # todate=input(request.args.get('todate'))
+    cmsyscode=6
+    fromdate='04-May-2023'
+    todate='05-Jun-2023'
     conn = pyodbc.connect(connection_string)
     cursor = conn.cursor()
-    cursor.execute("EXEC DemoData @cmsyscode= ?,@fromdate= ?,@todate= ?", (cmsyscode,fromdate,todate))
+    cursor.execute("EXEC DemoData @cmsyscode= ?,@fromdate= ?,@todate= ?", (cmsyscode,fromdate,todate))  
     result = cursor.fetchall() 
     columns = [column[0] for column in cursor.description]
     result_reshaped = [tuple(row) for row in result]
-    df = pd.DataFrame(result_reshaped, columns=columns)
+    df= pd.DataFrame(result_reshaped, columns=columns)
     cursor.close()
     conn.close()
+
 
     fig = make_subplots(rows=1, cols=1, vertical_spacing=0.1, subplot_titles=(
         'Leads Status',
@@ -40,8 +45,13 @@ def main():
     fig_dict = fig.to_dict()
     fig_dict['data'][0]['x'] = fig_dict['data'][0]['x'].tolist()
 
+    # fig_html = py.plot(fig, output_type='div', include_plotlyjs='cdn')
+    # fig_html = py.plot(fig, output_type='div', include_plotlyjs='cdn', config={'displayModeBar': False})
+    # return fig_html 
+
     response = {'data': fig_dict}
     return jsonify(response)
 
 if __name__ == '__main__':
     app.run(debug=True)
+
