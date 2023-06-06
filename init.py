29a77a5e63d8883788b7fd43b08970ead6168aa4
Thinkiomkar <132989245+Thinkiomkar@users.noreply.github.com>
2023-06-06 13:15:01 +0530

    Update init.py

--- init.py
+++ init.py
@@ -1,13 +1,12 @@
-
+	
+	
+	
 from flask import Flask, jsonify,request
 import pyodbc
 import pandas as pd
-import numpy as np
 import plotly.offline as py
 import plotly.graph_objects as go
 from plotly.tools import make_subplots
-
-import plotly.subplots as sp
 
 
 server = 'ttplsqleu.database.windows.net'
@@ -22,9 +21,9 @@ def main():
     cmsyscode =int(request.args.get('cmsyscode'))
     fromdate=str(request.args.get('fromdate'))
     todate=str(request.args.get('todate'))
-#     cmsyscode=6
-#     fromdate='04-May-2023'
-#     todate='05-Jun-2023'
+    # cmsyscode=6
+    # fromdate='04-May-2023'
+    # todate='05-Jun-2023'
     conn = pyodbc.connect(connection_string)
     cursor = conn.cursor()
     cursor.execute("EXEC DemoData @cmsyscode= ?,@fromdate= ?,@todate= ?", (cmsyscode,fromdate,todate))  
@@ -34,32 +33,30 @@ def main():
     df= pd.DataFrame(result_reshaped, columns=columns)
     cursor.close()
     conn.close()
-     
-    # df.replace({np.NaN:None},inplace=True)
- 
-    fig = make_subplots(rows=2, cols=2, vertical_spacing=0.1, subplot_titles=(
-        'Leads Status','Table1','Lead Source','Table2',
-    ))
 
+
+    fig = make_subplots(rows=2, cols=1, vertical_spacing=0.1, subplot_titles=(
+        'Leads Status','Lead Source'
+    ))
     T1 = go.Histogram(x=df['ls_description'])
     T1.name = 'Leads Status'
     fig.add_trace(T1, row=1, col=1)
     fig.update_layout(
-        height=1000, 
-        width=1000,
-        paper_bgcolor="LightSteelBlue"    
-    )  
-    table1 = df.to_dict(orient='records')
-    
-    
+        height=800, 
+        width=1000  
+    )
+
     T2 = go.Histogram(x=df['COL_ContactLabel'],)
     T2.name='Lead Source'
-    fig.add_trace(T2,row=2, col=1)
+    fig.add_trace(T2,row=2,col=1)
+    T2=go.histogram.Marker(color="blue")
     fig.update_layout(
-        height=1000, width=1000,
+        height=800, width=1000,
         margin=dict(l=50, r=50, t=100, b=100) ,paper_bgcolor="LightSteelBlue"  
+        # legend_tracegroupgap=10,
+        # legend_font_color="black",
+        # legend_font_size=10
     )
-    table2 = df.to_dict(orient='records')
 
     fig_dict = fig.to_dict()
     fig_dict['data'][0]['x'] = fig_dict['data'][0]['x'].tolist()
@@ -69,8 +66,8 @@ def main():
     # fig_html = py.plot(fig, output_type='div', include_plotlyjs='cdn', config={'displayModeBar': False})
     # return fig_html 
 
-    response = {'data': fig_dict,'table1': pd.DataFrame(table1).to_html(index=False),'table2': pd.DataFrame(table2).to_html(index=False)}
-    return jsonify(response)  
+    response = {'data': fig_dict}
+    return jsonify(response)
 
 if __name__ == '__main__':
     app.run(debug=True)
