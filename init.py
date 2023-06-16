2de9e750484b505d2a50ecd45ef91df702652f41
Thinkiomkar <132989245+Thinkiomkar@users.noreply.github.com>
2023-06-16 14:44:05 +0530

    Update init.py

--- init.py
+++ init.py
@@ -1,15 +1,13 @@
-
 from flask import Flask, jsonify,request
+from plotly.tools import make_subplots
 import pyodbc
 import pandas as pd
 import numpy as np
 import plotly.offline as py
 import plotly.graph_objects as go
-from plotly.tools import make_subplots
-import plotly.subplots as sp
 import plotly.express as px
 import config
-import json
+import Parameters
 
 connection_string = config.SQL_CONNECTION_STRING
 app = Flask(__name__)
@@ -18,32 +16,28 @@ def main():
     cmsyscode =str(request.args.get('cmsyscode'))
     fromdate=str(request.args.get('fromdate'))
     todate=str(request.args.get('todate'))
+	um_user_syscode=str(request.args.get('um_user_syscode'))
     conn = pyodbc.connect(connection_string)
     cursor = conn.cursor() 
-    cursor.execute("EXEC DemoData @cmsyscode= ?,@fromdate= ?,@todate= ?", (cmsyscode,fromdate,todate))   
+    cursor.execute("EXEC DemoData @cmsyscode= ?,@fromdate= ?,@todate= ?,@um_user_syscode=?", (cmsyscode,fromdate,todate,um_user_syscode))   
     result = cursor.fetchall() 
     columns = [column[0] for column in cursor.description]
     result_reshaped = [tuple(row) for row in result]
     df= pd.DataFrame(result_reshaped, columns=columns)
     cursor.close()
     conn.close()
-    fig = make_subplots(rows=1, cols=2, vertical_spacing=0.15, subplot_titles=(
-        'Lead Status','Lead Source',
-    ))
+
+    fig = make_subplots(rows=1, cols=2, vertical_spacing=0.15, subplot_titles=('<b>Lead Status<b>','<b>Lead Source<b>',)
     T1 = go.Histogram(x=df['ls_description'],marker_color='#0bb4c6')
-    T1.name = ''                                           # remove trace name display
-    # T1.showlegend=False                                   #to remove trace colour box
+    T1.name = ''                                                                           
     fig.add_trace(T1, row=1, col=1)
     fig.update_layout(
         height=400, 
         width=1330 ,
-        margin=dict(l=50, r=50, t=100, b=100) 
-    )
+        margin=dict(l=50, r=50, t=100, b=100))
     T2 = go.Histogram(x=df['COL_ContactLabel'],marker_color='#8BC34A')
     T2.name=''
-    # T2.showlegend=False
     fig.add_trace(T2,row=1,col=2)
-
     xaxis_labels =df['COL_ContactLabel'].unique()
     updated_labels = []
     for label in xaxis_labels:
@@ -51,7 +45,7 @@ def main():
             label_parts = label.split(' ')
             updated_labels.append('<br>'.join(label_parts))
         else:
-            updated_labels.append(label)          
+            updated_labels.append(label)         
     fig.update_layout(
         height=400, width=1350,#h=400 
         margin=dict(l=50, r=50, t=50, b=50),
@@ -66,14 +60,9 @@ def main():
             ticktext=updated_labels,
             showticklabels=True,
             tickfont=dict(size=12),
-            automargin=True,  
-    )  
-    ) 
-    fig.update_layout(
-    modebar={'remove': ['zoom2d', 'autoscale2d', 'pan2d', 'lasso2d', 'resetScale2d','displaylogo','ModeBar'],
-              },
-    showlegend=False,
-)
+            automargin=True,))
+    fig.update_annotations(font_size=16)
+    fig.update_layout(modebar={'remove': ['zoom2d', 'autoscale2d', 'pan2d', 'lasso2d', 'resetScale2d','logo','ModeBar'],},showlegend=False,)
     fig_dict = fig.to_dict()
     fig_dict['data'][0]['x'] = fig_dict['data'][0]['x'].tolist()
     fig_dict['data'][1]['x'] = fig_dict['data'][1]['x'].tolist()
@@ -85,34 +74,42 @@ def main3():
     cmsyscode =str(request.args.get('cmsyscode'))
     fromdate=str(request.args.get('fromdate'))
     todate=str(request.args.get('todate'))
+    um_user_syscode=str(request.args.get('um_user_syscode'))
     conn = pyodbc.connect(connection_string)
     cursor = conn.cursor() 
-    cursor.execute("EXEC DemoData @cmsyscode= ?,@fromdate= ?,@todate= ?", (cmsyscode,fromdate,todate))   
+    cursor.execute("EXEC DemoData @cmsyscode= ?,@fromdate= ?,@todate= ?,@um_user_syscode=?", (cmsyscode,fromdate,todate,um_user_syscode))   
     result = cursor.fetchall() 
     columns = [column[0] for column in cursor.description]
     result_reshaped = [tuple(row) for row in result]
     df= pd.DataFrame(result_reshaped, columns=columns)
     cursor.close()
-    conn.close()
-    fig = make_subplots(rows=1, cols=1, vertical_spacing=0.15, subplot_titles=(
-        'Lead Status',
-    ))
+    conn.close()	
+    color_map = {
+   'Lost Lead': 'rgb(255, 0, 0)',    
+    'Closed': '#4CAF50',    
+    'WIP': '#FF9800', 
+    'Junk Lead' :'gray',
+    'Contact in Future':' #BA68C8',}
+    df['color'] = df['ls_description'].map(color_map)
     counts = df.groupby(['COL_ContactLabel', 'ls_description']).size().reset_index(name='count')
     fig = px.bar(counts, x='COL_ContactLabel', y='count', color='ls_description', 
-             title=' % Of  Leads Conversions From Sources', barmode='group', text_auto='ls_description')
-    fig.update_traces(textfont_size=18, textangle=0, textposition="outside", cliponaxis=False,  hovertemplate='%{x}<br><b>%{y}</b>') # hovertemplate are used to adjust which data has to be shown when we hover mouse cursor
-    fig.update_layout(xaxis=dict(title='Sources'),
-                  yaxis=dict(title='Total'),
-                legend=dict(title='Lead Status'),
-                title=dict(text="<br>% Of  Leads Conversions From Sources</br>"),
-                 height=600, width=1350,     
-                )
-    fig.update_layout(   showlegend=True,    
- )
-    fig_html = fig.to_html(full_html=False)
-    fig_html = fig_html.replace('<head>', '<head><style>.plotlyjsmodebar{display:none!important;}</style>')
-    response = {'data': fig_html}
+             title=' % Of  Leads Conversions From Sources', barmode='group', text_auto='ls_description')  
+    for ls_description, color in color_map.items():
+        fig.for_each_trace(lambda t: t.update(marker_color=color) if t.name == ls_description else ())
+    fig.update_traces(textfont_size=12, textangle=0, textposition="outside", cliponaxis=False,  hovertemplate='%{x}<br><b>%{y}</b>') 
+    fig.update_layout(xaxis=dict(title='<b>Sources<b>'),
+                  yaxis=dict(title='<b>Count<b>'),
+                legend=dict(title='<b>Lead Status<b>'),
+                title=dict(text="<b>Lead Conversions From Sources<b>"),
+                plot_bgcolor='white',
+                 height=600, width=1350,
+                 font_size=14)
+    fig.update_layout(modebar={'remove': ['zoom2d', 'autoscale2d', 'pan2d', 'lasso2d', 'resetScale2d','logo','ModeBar']},showlegend=True,)
+    fig_json = fig.to_json()
+    response = {'data': fig_json}
     return jsonify(response)
-
+	
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True)	
+
+
